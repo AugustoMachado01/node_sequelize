aa23ec09df0c1872e67e248122a03f66df1e5536
AugustoMachado01 <augustomachadochieno01@gmail.com>
2024-10-21 11:56:38 -0400

    remove data

--- index.js
+++ index.js
@@ -15,14 +15,6 @@ app.set("view engine", "handlebars");
 
 app.use(express.static("public"));
 
-app.get("/", async (req, res) => {
-  const users = await User.findAll({ raw: true });
-
-  console.log(users);
-
-  res.render("home", { users });
-});
-
 app.get("/users/create", (req, res) => {
   res.render("adduser");
 });
@@ -34,15 +26,33 @@ app.post("/users/create", (req, res) => {
 
   if (newsletter == "on") {
     newsletter = true;
-  } else {
-    newsletter = false;
   }
-
-  console.log(req.body);
 
   User.create({ name, occupation, newsletter });
 
   res.redirect("/");
+});
+
+app.get("/users/:id", async (req, res) => {
+  const id = req.params.id;
+
+  const user = await User.findOne({ raw: true, where: { id: id } });
+
+  res.render("userview", { user });
+});
+
+app.post("/users/delete/:id", async (req, res) => {
+  const id = req.params.id;
+
+  await User.destroy({ where: { id: id } });
+
+  res.redirect("/");
+});
+
+app.get("/", async (req, res) => {
+  const users = await User.findAll({ raw: true });
+
+  res.render("home", { users });
 });
 
 conn
